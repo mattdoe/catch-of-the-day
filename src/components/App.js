49a47d89099759d214fd940fc4d7e01857221e63
Matt Doe <doe.matthew@gmail.com>
2018-10-21 13:26:17 -0400

    Pass props to Header to define tagline

--- src/components/App.js
+++ src/components/App.js
@@ -7,8 +7,8 @@ class App extends Component {
     render() {
         return (
             <div className='catch-of-the-day'>
-                <div className="menu">
-                    <Header />
+                <div className='menu'>
+                    <Header tagline='Fresh Seafood Market' />
                 </div>
                 <Order />
                 <Inventory />
