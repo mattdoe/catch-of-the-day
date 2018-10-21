c573cc5fa8b97142597cd7742005cf8c07b0043c
Matt Doe <doe.matthew@gmail.com>
2018-10-21 14:27:04 -0400

    Add form submit event handler

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,15 +2,23 @@ import React, { Component } from 'react';
 import { getFunName } from "../helpers";
 
 class StorePicker extends Component {
+
+    myInput = React.createRef();
+
+    goToStore = (event) => {
+        event.preventDefault();
+        console.log(this.myInput);
+    };
+
     render() {
         return (
-            <form className='store-selector'>
+            <form className='store-selector' onSubmit={this.goToStore}>
                 <h2>Please Enter A Store</h2>
-                <input type='text' required placeholder='Store Name' defaultValue={getFunName()}/>
+                <input type='text' ref={this.myInput} required placeholder='Store Name' defaultValue={getFunName()} />
                 <button type='submit'>Visit Store →</button>
             </form>
         );
-    }
+    };
 }
 
 export default StorePicker;
