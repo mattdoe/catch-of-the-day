f364639bb72e7454fd7cb52e0117a8e6f0698938
Matt Doe <doe.matthew@gmail.com>
2018-10-28 13:18:31 -0400

    Move Firebase details to safe config file that will not be stored publicly

--- src/base.js
+++ src/base.js
@@ -1,11 +1,8 @@
 import Rebase from 're-base';
 import firebase from 'firebase';
+import config from './config';
 
-const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyCRp39vV078eB1JQnk4XEYlbvEZH645upE",
-    authDomain: "catch-of-the-day-matt-doe.firebaseapp.com",
-    databaseURL: "https://catch-of-the-day-matt-doe.firebaseio.com"
-});
+const firebaseApp = firebase.initializeApp(config.firebase);
 
 const base = Rebase.createClass(firebaseApp.database());
 
